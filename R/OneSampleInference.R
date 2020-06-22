e96a93bc5a1058b3d2fd8d329529da399133259a
Harrindy <deweiwang@stat.sc.edu>
2020-06-22 14:15:12 -0400

    Chapter8

--- R/OneSampleInference.R
+++ R/OneSampleInference.R
@@ -1,11 +1,11 @@
-Zinterval.data=function(x,sigma,level=0.95)
+Zinterval.data=function(data,sigma,level=0.95)
 {
   if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
   alpha=1-level
   z_a=qnorm(1-alpha/2)
-  n=length(x)
-  barx=mean(x)
-  cat("The sample mean is", mean(x),"and sample size is", n,"\n")
+  n=length(data)
+  barx=mean(data)
+  cat("The sample mean is", mean(data),"and sample size is", n,"\n")
   cat("A", level*100, "% two-sided confidence interval for the population mean is [", barx-z_a*sigma/sqrt(n),",",barx+z_a*sigma/sqrt(n),"]","\n")
   z_a=qnorm(1-alpha)
   cat("A", level*100, "% upper-confidence bound for the population mean is", barx+z_a*sigma/sqrt(n),"\n")
@@ -30,19 +30,19 @@ sample.size.Zinterval=function(E,sigma,level=0.95)
   alpha=1-level
   z_a=qnorm(1-alpha/2)
   n=ceiling((z_a*sigma/E)^2)
-  cat("The samllest sample size to control the margin of error E <=", E, "at",level*100, "% confidence level is", n, "\n")
+  cat("In the estimation of mu, the samllest sample size to control the margin of error E <=", E, "at",level*100, "% confidence level is", n, "\n")
 }
 
-AZinterval.data=function(x,level=0.95)
+AZinterval.data=function(data,level=0.95)
 {
-  s=sd(x)
+  s=sd(data)
   if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
   alpha=1-level
   z_a=qnorm(1-alpha/2)
-  n=length(x)
-  barx=mean(x)
-  cat("The sample mean is", mean(x),", sample standard deviation is", s, ", and sample size is", n,"\n")
-  cat("A large-sample confidence interval for the population mean with confidence level of approximately", level*100, "% is [", barx-z_a*s/sqrt(n),",",mean(x)+z_a*sigma/sqrt(n),"]","\n")
+  n=length(data)
+  barx=mean(data)
+  cat("The sample mean is", mean(data),", sample standard deviation is", s, ", and sample size is", n,"\n")
+  cat("A large-sample confidence interval for the population mean with confidence level of approximately", level*100, "% is [", barx-z_a*s/sqrt(n),",",barx+z_a*s/sqrt(n),"]","\n")
   z_a=qnorm(1-alpha)
   cat("A large-sample upper-confidence bound for the population mean with confidence level of approximately", level*100, "% is", barx+z_a*s/sqrt(n),"\n")
   cat("A large-sample lower-confidence bound for the population mean with confidence level of approximately", level*100, "% is", barx-z_a*s/sqrt(n),"\n")
@@ -53,8 +53,8 @@ AZinterval.stat=function(n,barx,s,level=0.95)
   if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
   alpha=1-level
   z_a=qnorm(1-alpha/2)
-  cat("The sample mean is", mean(x),", sample standard deviation is", s, ", and sample size is", n,"\n")
-  cat("A large-sample confidence interval for the population mean with confidence level of approximately", level*100, "% is [", barx-z_a*s/sqrt(n),",",mean(x)+z_a*sigma/sqrt(n),"]","\n")
+  cat("The sample mean is", barx,", sample standard deviation is", s, ", and sample size is", n,"\n")
+  cat("A large-sample confidence interval for the population mean with confidence level of approximately", level*100, "% is [", barx-z_a*s/sqrt(n),",",barx+z_a*s/sqrt(n),"]","\n")
   z_a=qnorm(1-alpha)
   cat("A large-sample upper-confidence bound for the population mean with confidence level of approximately", level*100, "% is", barx+z_a*s/sqrt(n),"\n")
   cat("A large-sample lower-confidence bound for the population mean with confidence level of approximately", level*100, "% is", barx-z_a*s/sqrt(n),"\n")
@@ -68,16 +68,16 @@ t.quantile=function(df,q)
 }
 
 
-Tinterval.data=function(x,level=0.95)
+Tinterval.data=function(data,level=0.95)
 {
   if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
   alpha=1-level
-  n=length(x)
+  n=length(data)
   t_a=t.quantile(df=n-1,1-alpha/2)
-  barx=mean(x)
-  s=sd(x)
+  barx=mean(data)
+  s=sd(data)
   cat("The sample mean is", barx,", sample standard deviation is", s, ", and sample size is", n,"\n")
-  cat("A", level*100, "% two-sided confidence interval for the population mean is [", barx-t_a*s/sqrt(n),",",bars+t_a*s/sqrt(n),"]","\n")
+  cat("A", level*100, "% two-sided confidence interval for the population mean is [", barx-t_a*s/sqrt(n),",",barx+t_a*s/sqrt(n),"]","\n")
   t_a=t.quantile(df=n-1,1-alpha)
   cat("A", level*100, "% upper-confidence bound for the population mean is", barx+t_a*s/sqrt(n),"\n")
   cat("A", level*100, "% lower-confidence bound for the population mean is", barx-t_a*s/sqrt(n),"\n")
@@ -93,4 +93,108 @@ Tinterval.stat=function(n,barx,s,level=0.95)
   t_a=t.quantile(df=n-1,1-alpha)
   cat("A", level*100, "% upper-confidence bound for the population mean is", barx+t_a*s/sqrt(n),"\n")
   cat("A", level*100, "% lower-confidence bound for the population mean is", barx-t_a*s/sqrt(n),"\n")
+}
+
+Chi2.quantile=function(df,q)
+{
+  if(df<=0){return("df must be positive")}
+  if(q<=0|q>=1){return("q must be between 0 and 1")}
+  return(qchisq(q,df))
+}
+
+Chi2interval.data=function(data,level=0.95)
+{
+  if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
+  alpha=1-level
+  n=length(data)
+  chi_a1=Chi2.quantile(df=n-1,1-alpha/2)
+  chi_a2=Chi2.quantile(df=n-1,alpha/2)
+  s2=var(data)
+  cat("The sample standard variance is", s2, "and sample size is", n,"\n")
+  cat("A", level*100, "% two-sided confidence interval for the population variance is [", (n-1)*s2/chi_a1,",",(n-1)*s2/chi_a2,"]","\n")
+  chi_a1=Chi2.quantile(df=n-1,1-alpha)
+  chi_a2=Chi2.quantile(df=n-1,alpha)
+  cat("A", level*100, "% upper-confidence bound for the population variance is", (n-1)*s2/chi_a2,"\n")
+  cat("A", level*100, "% lower-confidence bound for the population variance is", (n-1)*s2/chi_a1,"\n","\n")
+
+  chi_a1=Chi2.quantile(df=n-1,1-alpha/2)
+  chi_a2=Chi2.quantile(df=n-1,alpha/2)
+  cat("The sample standard deviation is", sqrt(s2), "and sample size is", n,"\n")
+  cat("A", level*100, "% two-sided confidence interval for the population standard deviation is [", sqrt((n-1)*s2/chi_a1),",",sqrt((n-1)*s2/chi_a2),"]","\n")
+  chi_a1=Chi2.quantile(df=n-1,1-alpha)
+  chi_a2=Chi2.quantile(df=n-1,alpha)
+  cat("A", level*100, "% upper-confidence bound for the population standard deviation is", sqrt((n-1)*s2/chi_a2),"\n")
+  cat("A", level*100, "% lower-confidence bound for the population standard deviation is", sqrt((n-1)*s2/chi_a1),"\n")
+}
+
+Chi2interval.stat=function(n,s,level=0.95)
+{
+  if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
+  alpha=1-level
+  chi_a1=Chi2.quantile(df=n-1,1-alpha/2)
+  chi_a2=Chi2.quantile(df=n-1,alpha/2)
+  s2=s^2
+  cat("The sample standard variance is", s2, "and sample size is", n,"\n")
+  cat("A", level*100, "% two-sided confidence interval for the population variance is [", (n-1)*s2/chi_a1,",",(n-1)*s2/chi_a2,"]","\n")
+  chi_a1=Chi2.quantile(df=n-1,1-alpha)
+  chi_a2=Chi2.quantile(df=n-1,alpha)
+  cat("A", level*100, "% upper-confidence bound for the population variance is", (n-1)*s2/chi_a2,"\n")
+  cat("A", level*100, "% lower-confidence bound for the population variance is", (n-1)*s2/chi_a1,"\n","\n")
+
+  chi_a1=Chi2.quantile(df=n-1,1-alpha/2)
+  chi_a2=Chi2.quantile(df=n-1,alpha/2)
+  cat("The sample standard deviation is", sqrt(s2), "and sample size is", n,"\n")
+  cat("A", level*100, "% two-sided confidence interval for the population standard deviation is [", sqrt((n-1)*s2/chi_a1),",",sqrt((n-1)*s2/chi_a2),"]","\n")
+  chi_a1=Chi2.quantile(df=n-1,1-alpha)
+  chi_a2=Chi2.quantile(df=n-1,alpha)
+  cat("A", level*100, "% upper-confidence bound for the population standard deviation is", sqrt((n-1)*s2/chi_a2),"\n")
+  cat("A", level*100, "% lower-confidence bound for the population standard deviation is", sqrt((n-1)*s2/chi_a1),"\n")
+}
+
+Propinterval=function(n,x,level=0.95)
+{
+  if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
+  p=x/n
+  alpha=1-level
+  z_a=qnorm(1-alpha/2)
+  cat("The sample proportion is", p,"and sample size is", n,"\n")
+  cat("A large-sample confidence interval for the population proportion with confidence level of approximately", level*100, "% is [", p-z_a*sqrt(p*(1-p)/n),",",p+z_a*sqrt(p*(1-p)/n),"]","\n")
+  z_a=qnorm(1-alpha)
+  cat("A large-sample upper-confidence bound for the population proportion with confidence level of approximately", level*100, "% is", p+z_a*sqrt(p*(1-p)/n),"\n")
+  cat("A large-sample lower-confidence bound for the population proportion with confidence level of approximately", level*100, "% is", p-z_a*sqrt(p*(1-p)/n),"\n")
+}
+
+sample.size.Propinterval=function(E,ini.p=0.5,level=0.95)
+{
+  if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
+  if(ini.p>=1|level<0){return("the initial estimate should be between 0 and 1!")}
+  n=ceiling((qnorm(1-(1-level)/2)/E)^2*ini.p*(1-ini.p))
+  cat("In the estimation of p, the samllest sample size to control the margin of error E <=", E, "at",level*100, "% confidence level is", n, "\n")
+}
+
+Predinterval.data=function(data,level=0.95)
+{
+  if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
+  alpha=1-level
+  n=length(data)
+  t_a=t.quantile(df=n-1,1-alpha/2)
+  barx=mean(data)
+  s=sd(data)
+  cat("The sample mean is", barx,", sample standard deviation is", s, ", and sample size is", n,"\n")
+  cat("A", level*100, "% two-sided predict interval for the next observation is [", barx-t_a*s*sqrt(1+1/n),",",barx+t_a*s*sqrt(1+1/n),"]","\n")
+  t_a=t.quantile(df=n-1,1-alpha)
+  cat("A", level*100, "% upper-confidence bound for the next observation is", barx+t_a*s*sqrt(1+1/n),"\n")
+  cat("A", level*100, "% lower-confidence bound for the next observation is", barx-t_a*s*sqrt(1+1/n),"\n")
+}
+
+Predinterval.stat=function(n,barx,s,level=0.95)
+{
+  if(level>=1|level<0){return("the confidence level should be between 0 and 1!")}
+  alpha=1-level
+  t_a=t.quantile(df=n-1,1-alpha/2)
+  cat("The sample mean is", barx,", sample standard deviation is", s, ", and sample size is", n,"\n")
+  cat("A", level*100, "% two-sided predict interval for the next observation is [", barx-t_a*s*sqrt(1+1/n),",",barx+t_a*s*sqrt(1+1/n),"]","\n")
+  t_a=t.quantile(df=n-1,1-alpha)
+  cat("A", level*100, "% upper-confidence bound for the next observation is", barx+t_a*s*sqrt(1+1/n),"\n")
+  cat("A", level*100, "% lower-confidence bound for the next observation is", barx-t_a*s*sqrt(1+1/n),"\n")
 }
